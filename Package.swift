082216c22d44a8b1e6ac485da0ca6f1f50881ef2
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-12 14:56:23 +0300

    1.8.0-1

---
+++ Package.swift
@@ -0,0 +1,332 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.8.0-1"
+
+let package = Package(
+    name: "BanubaSdk",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BanubaSdk",
+            targets: ["BanubaSdk"]
+        ),
+        .library(
+            name: "BNBSdkCore",
+            targets: ["BNBSdkCore"]
+        ),
+        .library(
+            name: "BNBSdkApi",
+            targets: ["BNBSdkApi"]
+        ),
+        .library(
+            name: "BNBEffectPlayer",
+            targets: ["BNBEffectPlayer"]
+        ),
+        .library(
+            name: "BNBScripting",
+            targets: ["BNBScripting"]
+        ),
+        .library(
+            name: "BNBFaceTracker",
+            targets: ["BNBFaceTracker"]
+        ),
+        .library(
+            name: "BNBFaceTrackerLite",
+            targets: ["BNBFaceTrackerLite"]
+        ),
+        .library(
+            name: "BNBLips",
+            targets: ["BNBLips"]
+        ),
+        .library(
+            name: "BNBHair",
+            targets: ["BNBHair"]
+        ),
+        .library(
+            name: "BNBHands",
+            targets: ["BNBHands"]
+        ),
+        .library(
+            name: "BNBWatch",
+            targets: ["BNBWatch"]
+        ),
+        .library(
+            name: "BNBOcclusion",
+            targets: ["BNBOcclusion"]
+        ),
+        .library(
+            name: "BNBEyes",
+            targets: ["BNBEyes"]
+        ),
+        .library(
+            name: "BNBSkin",
+            targets: ["BNBSkin"]
+        ),
+        .library(
+            name: "BNBBackground",
+            targets: ["BNBBackground"]
+        ),
+        .library(
+            name: "BNBBody",
+            targets: ["BNBBody"]
+        ),
+        .library(
+            name: "BNBAcneEyebagsRemoval",
+            targets: ["BNBAcneEyebagsRemoval"]
+        ),
+        .library(
+            name: "BNBPoseEstimation",
+            targets: ["BNBPoseEstimation"]
+        ),
+        .library(
+            name: "BNBNeurobeautyMakeup",
+            targets: ["BNBNeurobeautyMakeup"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBSdkApi.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBEffectPlayer.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBScripting.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBFaceTracker.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBFaceTrackerLite.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBLips.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBHair.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBHands.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBWatch.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBOcclusion.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBEyes.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBSkin.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBBackground.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBBody.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBAcneEyebagsRemoval.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBPoseEstimation.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBNeurobeautyMakeup.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BanubaSdk",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BanubaSdk.zip",
+            checksum: "cfb1ddb256c4aa1dc3ca5be4447e924a67cdd2f0fcf309cdb456caf481e99e1b"
+        ),
+        .target(
+            name: "BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBSdkApi",
+            dependencies: [
+                .product(
+                    name: "BNBSdkApi",
+                    package: "BNBSdkApi"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBEffectPlayer",
+            dependencies: [
+                .product(
+                    name: "BNBEffectPlayer",
+                    package: "BNBEffectPlayer"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBScripting",
+            dependencies: [
+                .product(
+                    name: "BNBScripting",
+                    package: "BNBScripting"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBFaceTracker",
+            dependencies: [
+                .product(
+                    name: "BNBFaceTracker",
+                    package: "BNBFaceTracker"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBFaceTrackerLite",
+            dependencies: [
+                .product(
+                    name: "BNBFaceTrackerLite",
+                    package: "BNBFaceTrackerLite"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBLips",
+            dependencies: [
+                .product(
+                    name: "BNBLips",
+                    package: "BNBLips"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBHair",
+            dependencies: [
+                .product(
+                    name: "BNBHair",
+                    package: "BNBHair"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBHands",
+            dependencies: [
+                .product(
+                    name: "BNBHands",
+                    package: "BNBHands"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBWatch",
+            dependencies: [
+                .product(
+                    name: "BNBWatch",
+                    package: "BNBWatch"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBOcclusion",
+            dependencies: [
+                .product(
+                    name: "BNBOcclusion",
+                    package: "BNBOcclusion"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBEyes",
+            dependencies: [
+                .product(
+                    name: "BNBEyes",
+                    package: "BNBEyes"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBSkin",
+            dependencies: [
+                .product(
+                    name: "BNBSkin",
+                    package: "BNBSkin"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBBackground",
+            dependencies: [
+                .product(
+                    name: "BNBBackground",
+                    package: "BNBBackground"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBBody",
+            dependencies: [
+                .product(
+                    name: "BNBBody",
+                    package: "BNBBody"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBAcneEyebagsRemoval",
+            dependencies: [
+                .product(
+                    name: "BNBAcneEyebagsRemoval",
+                    package: "BNBAcneEyebagsRemoval"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBPoseEstimation",
+            dependencies: [
+                .product(
+                    name: "BNBPoseEstimation",
+                    package: "BNBPoseEstimation"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBNeurobeautyMakeup",
+            dependencies: [
+                .product(
+                    name: "BNBNeurobeautyMakeup",
+                    package: "BNBNeurobeautyMakeup"
+                ),
+            ]
+        ),
+    ]
+)
